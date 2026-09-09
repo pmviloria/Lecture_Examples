c0e236ca13cc802cb072628a0afb96b079fd53d2
pmviloria <pmviloria@carc.pshs.edu.ph>
2026-09-09 15:52:19 +0800

    Create multiple_Example1.py

---
+++ SG5/multiple_Example1.py
@@ -0,0 +1,11 @@
+grade = float(input("Enter your grade: "))
+if grade >= 90:
+    print("Excellent")
+elif grade >= 85:
+    print("Very Good")
+elif grade >= 80:
+    print("Good")
+elif grade >= 75:
+    print("Passed")
+else:
+    print("Failed")
