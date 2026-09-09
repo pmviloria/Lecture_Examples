c699b5197f55b3632a534cff77613c0ddd9a2896
pmviloria <pmviloria@carc.pshs.edu.ph>
2026-09-09 15:43:26 +0800

    Update nestedif_example1.py

--- SG5/nestedif_example1.py
+++ SG5/nestedif_example1.py
@@ -1,8 +1,8 @@
 grade = float(input("Enter your grade: "))
 if 0 <= grade <= 100:
-if grade >= 75:
-print("Passed")
+    if grade >= 75:
+        print("Passed")
+    else:
+        print("Failed")
 else:
-print("Failed")
-else:
-print("Invalid Grade")
+    print("Invalid Grade")
