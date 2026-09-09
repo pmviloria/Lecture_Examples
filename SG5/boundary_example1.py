8693f0226e178d56c0de2717b5369e994c41da9b
pmviloria <pmviloria@carc.pshs.edu.ph>
2026-09-09 15:50:02 +0800

    Create boundary_example1.py

---
+++ SG5/boundary_example1.py
@@ -0,0 +1,6 @@
+exam_score = float(input("Enter exam score: "))
+interview_score = float(input("Enter interview score: "))
+if exam_score >= 85 and interview_score >= 80:
+  print("Qualified")
+else:
+  print("Not Qualified")
