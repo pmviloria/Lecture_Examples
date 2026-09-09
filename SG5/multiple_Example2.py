7f4548336336981fb9ba32953a3aebfe88e13700
pmviloria <pmviloria@carc.pshs.edu.ph>
2026-09-09 15:53:38 +0800

    Create multiple_Example2.py

---
+++ SG5/multiple_Example2.py
@@ -0,0 +1,9 @@
+temperature = float(input("Enter temperature: "))
+if temperature >= 35:
+    print("Very Hot")
+elif temperature >= 30:
+    print("Hot")
+elif temperature >= 20:
+    print("Comfortable")
+else:
+    print("Cold")
